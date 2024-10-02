00886bf5aefdabbf9b8f4b55bebabac778803433
ceesvanspaendonck <ceesvanspaendonck@gmail.com>
2024-10-02 15:02:14 +0200

    Fix overdraw savings test

--- tests/account_tests.py
+++ tests/account_tests.py
@@ -40,7 +40,7 @@ def test_overdraw_checking():
 def test_overdraw_savings():
     account = Account('savings')
     account.deposit(50)
-    account.withdraw(100)
-    assert account.balance == -50
+    account.withdraw(25)
+    assert account.balance == 25
 
  # TO DO: fix overdraw savings test
